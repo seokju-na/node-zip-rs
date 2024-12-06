eb0a3c2a9810b2e7dbe22023cf6925309e2accbc
Seokju Na <seokju.me@toss.im>
2024-12-06 22:58:35 +0900

    fix test

--- tests/write.spec.ts
+++ tests/write.spec.ts
@@ -7,6 +7,7 @@ import { ROOT_DIR } from './env';
 test('write zip file', async () => {
   const srcDir = path.join(ROOT_DIR, 'tests', 'fixtures', 'simple');
   const dst = path.join(ROOT_DIR, 'tmp', 'simple.zip');
+  await fs.mkdir(path.dirname(dst), { recursive: true });
   await writeAsync(srcDir, dst);
   await expect(fs.access(dst)).resolves.toBeUndefined();
 });
